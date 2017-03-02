ad30f828481fd743366b9f85cc22470ea70467b0
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-02 15:29:19 -0600

    Added Row component

--- public/js/main.js
+++ public/js/main.js
@@ -3,26 +3,54 @@
 var Table = (function(){
 	var $instance = {};
 
-	$instance.data = [];
+	$instance.rows = [];
 	$instance.load = function(data){
 		var table = this;
-		table.rows = data;
+		data.forEach(table.appendRow.bind(table));
 		return table;
+	}
+	$instance.appendRow = function(data){
+		var table = this;
+		var row = Row.create(data);
+		table.rows.push(row);
 	}
 	$instance.view = function(){
 		var table = this;
 		return m('table', [
 			table.rows.map(function(row){
 				return m('tr', [
-					row.map(function(cell){
-						return m('td', cell);
-					})
+					row.view()
 				]);
 			})
 		]);
 	}
 
 	return $instance;
+})();
+
+var Row = (function(){
+	var $Class = {};
+	var $instance = {};
+
+	$Class.create = function(){
+		var row = Object.create($instance);
+		row.construct.apply(row, arguments);
+		return row;
+	}
+
+	$instance.cells = [];
+	$instance.construct = function(data){
+		var row = this;
+		row.cells = data;
+	}
+	$instance.view = function(){
+		var row = this;
+		return row.cells.map(function(cell){
+			return m('td', cell);
+		});
+	}
+
+	return $Class;
 })();
 
 document.addEventListener('DOMContentLoaded', function(){
