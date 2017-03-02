6b1bad1815b67feb567912b70ab3d87535aa208a
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-02 15:06:35 -0600

    Renders table

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,21 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function(){
+	var data = [
+		['a','b','c','d','e'],
+		['1','2','3','4','5']
+	];
 	m.mount(document.getElementById('app'), {
 		view: function(){
-			return m('p', 'This is Mithril!');
+			return m('table', [
+				data.map(function(row){
+					return m('tr', [
+						row.map(function(cell){
+							return m('td', cell);
+						})
+					]);
+				})
+			]);
 		}
 	});
 });
