afc52d2fa258295bd776f059dc8eaccb5340b09b
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-02 15:14:02 -0600

    Table as separate component

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,24 @@
 'use strict';
 
+var Table = {
+	view: function(){
+		var table = this;
+		return m('table', [
+			table.data.map(function(row){
+				return m('tr', [
+					row.map(function(cell){
+						return m('td', cell);
+					})
+				]);
+			})
+		]);
+	}
+};
+
 document.addEventListener('DOMContentLoaded', function(){
-	var data = [
+	Table.data = [
 		['a','b','c','d','e'],
 		['1','2','3','4','5']
 	];
-	m.mount(document.getElementById('app'), {
-		view: function(){
-			return m('table', [
-				data.map(function(row){
-					return m('tr', [
-						row.map(function(cell){
-							return m('td', cell);
-						})
-					]);
-				})
-			]);
-		}
-	});
+	m.mount(document.getElementById('app'), Table);
 });
